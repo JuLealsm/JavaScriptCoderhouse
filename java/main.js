389a93ac2266f2b785d6e3b468dc9b15af9bf078
juliana <julianalealsm@hotmail.com>
2025-01-27 10:31:48 -0300

    1 - ajustanto array de plantas para reducir el numero de resultados null en la function resultado. 2 - Se anadio un <p> en el HTML para explicar minimamente como funciona el simulador para el usuario

--- java/main.js
+++ java/main.js
@@ -1,20 +1,25 @@
 
 
-const plantasSombra = ["Helecho", "Calathea", "Ficus", "Camelia"];
-const plantasSol = ["Hierba Limon", "Lavanda", "Romero", "Girasol"];
-const plantasSombraParcial = ["Hortencia", "Begonia", "Palma Areca", "Hosta"];
+const plantasSombra = ["Helecho", "Calathea", "Ficus", "Camelia", "Peperomia"];
+const plantasSol = ["Hierba Limon", "Lavanda", "Romero", "Girasol", "Planta del Dinero"];
+const plantasSombraParcial = ["Magnolia", "Begonia", "Palma Areca", "Hosta",  "Orquídea Mariposa", "Calathea orbifolia"];
 
-/*plantas pequenas: plantasSombra["Helecho", "Calathea"]
-                    plantasSol ["Romero", "Girasol"]
-                    plantasSombraParcial ["Hosta"]
-plantas con flores: plantasSombra[Camelia]
+/*plantas pequenas: plantasSombra["Helecho", "Calathea","Peperomia" ]
+                    plantasSol ["Romero", "Girasol", "Planta del Dinero"]
+                    plantasSombraParcial ["Hosta",  "Orquídea Mariposa", "Calathea orbifolia"];
+plantas con flores: plantasSombra[Camelia, "Peperomia"]
                     plantasSol ["Lavanda", "Romero", "Girasol"]
-                    plantasSombraParcial ["Hortencia", "Begonia",]
-plantas no toxicas: plantasSombra["Helecho", "Calathea"]
-                    plantasSol ["Romero", "Girasol"]
-                    plantasSombraParcial ["Palma Areca"]*/
+                    plantasSombraParcial ["Magnolia", "Begonia", "Orquídea Mariposa"]
+plantas no toxicas: plantasSombra["Helecho", "Calathea", "Peperomia"]
+                    plantasSol ["Romero", "Girasol", "Planta del Dinero"]
+                    plantasSombraParcial ["Magnolia", "Palma Areca", "Orquídea Mariposa","Calathea orbifolia"];]*/
 
-const preguntas = [ "El lugar deseado para colocar la planta recibe más de 8 horas de luz solar al día?","El lugar deseado para colocar la planta recibe al menos de 4 horas de luz solar al día?", "Prefieres una planta pequeña?", "Te gustaría tener una planta que produzca flores?", "Tienes una mascota que tenga acceso a la planta?" ];
+const preguntas = [ 
+    "El lugar deseado para colocar la planta recibe más de 8 horas de luz solar al día?",
+    "El lugar deseado para colocar la planta recibe al menos de 4 horas de luz solar al día?", 
+    "Prefieres una planta pequeña?", "Te gustaría tener una planta que produzca flores?", 
+    "Tienes una mascota que tenga acceso a la planta?" 
+];
 
 let respuestas = [];
 
@@ -47,9 +52,9 @@ function resultado() {
     }else if (respuestas[0]=== "si" && respuestas[2]=== "si" && respuestas[3] === "si" && respuestas[4] === "no"){ //plantas sol
         return [plantasSol[1], plantasSol[2]];
     }else if (respuestas[0]=== "si" && respuestas[2]=== "si" && respuestas[3] === "no" && respuestas[4] === "si"){ //plantas sol
-        return null;
+        return plantasSol[4];
     }else if (respuestas[0]=== "si" && respuestas[2]=== "si" && respuestas[3] === "no" && respuestas[4] === "no"){ //plantas sol
-        return null;
+        return plantasSol[4];
     }else if (respuestas[0]=== "si" && respuestas[2]=== "no" && respuestas[3] === "si" && respuestas[4] === "si"){ //plantas sol
         return plantasSol[3];
     }else if (respuestas[0]=== "si" && respuestas[2]=== "no" && respuestas[3] === "si" && respuestas[4] === "no"){ //plantas sol
@@ -59,15 +64,15 @@ function resultado() {
     }else if (respuestas[0]=== "si" && respuestas[2]=== "no" && respuestas[3] === "no" && respuestas[4] === "no"){ //plantas sol
         return plantasSol[0];
     }else if (respuestas[0]=== "no" && respuestas[1]=== "si" && respuestas[2]=== "si" && respuestas[3] === "si" && respuestas[4] === "si"){ //plantas de sombra parcial
-        return null;
+        return plantasSombraParcial[4];
     }else if (respuestas[0]=== "no" && respuestas[1]=== "si" && respuestas[2]=== "si" && respuestas[3] === "si" && respuestas[4] === "no"){ //plantas de sombra parcial
         return plantasSombraParcial[1];
     }else if (respuestas[0]=== "no" && respuestas[1]=== "si" && respuestas[2]=== "si" && respuestas[3] === "no" && respuestas[4] === "si"){ //plantas de sombra parcial
-        return null;
+        return plantasSombraParcial[5];
     }else if (respuestas[0]=== "no" && respuestas[1]=== "si" && respuestas[2]=== "si" && respuestas[3] === "no" && respuestas[4] === "no"){ //plantas de sombra parcial
         return plantasSombraParcial[3];
     }else if (respuestas[0]=== "no" && respuestas[1]=== "si" && respuestas[2]=== "no" && respuestas[3] === "si" && respuestas[4] === "si"){ //plantas de sombra parcial
-        return null;
+        return plantasSombraParcial[1];
     }else if (respuestas[0]=== "no" && respuestas[1]=== "si" && respuestas[2]=== "no" && respuestas[3] === "si" && respuestas[4] === "no"){ //plantas de sombra parcial
         return plantasSombraParcial[0];
     }else if (respuestas[0]=== "no" && respuestas[1]=== "si" && respuestas[2]=== "no" && respuestas[3] === "no" && respuestas[4] === "si"){ //plantas de sombra parcial
@@ -75,9 +80,9 @@ function resultado() {
     }else if (respuestas[0]=== "no" && respuestas[1]=== "si" && respuestas[2]=== "no" && respuestas[3] === "no" && respuestas[4] === "no"){ //plantas de sombra parcial
         return plantasSombraParcial[2];
     }else if (respuestas[0]=== "no" && respuestas[1]=== "no" && respuestas[2]=== "si" && respuestas[3] === "si" && respuestas[4] === "si"){ //plantas de sombra
-        return null;
+        return plantasSombra[4];
     }else if (respuestas[0]=== "no" && respuestas[1]=== "no" && respuestas[2]=== "si" && respuestas[3] === "si" && respuestas[4] === "no"){ //plantas de sombra
-        return null;
+        return plantasSombra[4];
     }else if (respuestas[0]=== "no" && respuestas[1]=== "no" && respuestas[2]=== "si" && respuestas[3] === "no" && respuestas[4] === "si"){ //plantas de sombra
         return [plantasSombra[0], plantasSombra[1]];
     }else if (respuestas[0]=== "no" && respuestas[1]=== "no" && respuestas[2]=== "si" && respuestas[3] === "no" && respuestas[4] === "no"){ //plantas de sombra
